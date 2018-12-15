75c3a5f9bb816e85969b5f832ac8480fd2cd9faa
Julie Gumerman <julie.gumerman@gmail.com>
2018-12-15 15:45:57 -0700

    latest lists

--- aire/src/list/list.ts
+++ aire/src/list/list.ts
@@ -1,12 +1,23 @@
 import {bindable, customElement, inject}      from "aurelia-framework";
-import {dom} from "aire/core";
+import {dom} from "src/core";
 
 @inject(Element)
 @customElement('aire-list')
 export class AireList {
     ul: HTMLElement;
 
+    @ bindable
+
+    lis     : string[];
+
+
     constructor(private element: Element){
 
+    }
+    attached() {
+        dom.decorateTo(this.element, this.ul, "divider", "uk-list-bullet");
+        dom.decorateTo(this.element, this.ul, "striped", "uk-list-divider");
+        dom.decorateTo(this.element, this.ul, "hover", "uk-list-striped");
+        dom.decorateTo(this.element, this.ul, "justify", "uk-list-large");
     }
 }
