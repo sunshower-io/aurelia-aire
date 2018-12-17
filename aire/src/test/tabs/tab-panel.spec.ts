d1eb38969e33fd1542fdeb3e349f023e460b8f73
Josiah Haswell <josiah.haswell@gmail.com>
2018-12-17 14:57:11 -0700

    testing tabs

---
+++ aire/src/test/tabs/tab-panel.spec.ts
@@ -0,0 +1,43 @@
+import 'reflect-metadata';
+import 'aurelia-polyfills';
+
+import {bootstrap}      from 'aurelia-bootstrapper';
+import {newComponent}         from "test/render";
+
+let component : any;
+
+
+
+afterEach(() => {
+  component.dispose();
+});
+
+
+
+test('tabs must be initializable', async () => {
+  component = newComponent(
+    `
+    aire-tab-panel
+    `,
+    {},
+    'tabs/tab-panel'
+  );
+  await component.create(bootstrap);
+
+  let tabEl = document.querySelector('.aire-tab-panel');
+  expect(tabEl).toBeTruthy();
+
+});
+
+test('a tab-panel must have tabs definable as children', async () => {
+  component = newComponent(`
+  aire-tab-panel
+    aire-tab(title.bind="frapper")
+      h1 \${frapper} 
+  `, {frapper: "wabbo"}, 'tabs/tab-panel', 'tabs/tab');
+
+  await component.create(bootstrap);
+
+  let h1 = document.querySelector('h1');
+  expect(h1.textContent).toBe("wabbo");
+});
