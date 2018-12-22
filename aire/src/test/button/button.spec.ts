c92c4b517be02c639bbd8186409b0029edd8d4e9
lisagumerman <lisajgumerman@gmail.com>
2018-12-21 21:08:10 -0700

    Button style tests

--- aire/src/test/button/button.spec.ts
+++ aire/src/test/button/button.spec.ts
@@ -10,8 +10,8 @@ import {render}         from "test/render";
 let component : any;
 
 
-function initialize(bindingContext : any) {
-  let template = `
+function initialize(bindingContext : any, templateString ?: string) {
+  let template = templateString ? templateString : `
   aire-button(label.bind="textLabel", icon.bind="icon")
   `;
   component = StageComponent.withResources(
@@ -46,4 +46,104 @@ test("a button have an icon when icon is not present in binding context", async 
   let button = document.querySelector('.uk-button');
   expect(button.firstElementChild).toBeTruthy();
   done();
+});
+
+test("a button must take a default argument", async(done) => {
+  initialize({textLabel: "Henlo"}, `
+aire-button(label.bind="textLabel", icon.bind="icon" default)
+  `);
+    await component.create(bootstrap);
+    let button = document.querySelector('.uk-button.uk-button-default');
+    expect(button).toBeTruthy();
+    done();
+});
+
+test("a button must take a primary argument", async(done) => {
+    initialize({textLabel: "Henlo"}, `
+aire-button(label.bind="textLabel", icon.bind="icon" primary)
+  `);
+    await component.create(bootstrap);
+    let button = document.querySelector('.uk-button.uk-button-primary');
+    expect(button).toBeTruthy();
+    done();
+});
+
+test("a button must take a secondary argument", async(done) => {
+    initialize({textLabel: "Henlo"}, `
+aire-button(label.bind="textLabel", icon.bind="icon" secondary)
+  `);
+    await component.create(bootstrap);
+    let button = document.querySelector('.uk-button.uk-button-secondary');
+    expect(button).toBeTruthy();
+    done();
+});
+
+test("a button must take a danger argument", async(done) => {
+    initialize({textLabel: "Henlo"}, `
+aire-button(label.bind="textLabel", icon.bind="icon" danger)
+  `);
+    await component.create(bootstrap);
+    let button = document.querySelector('.uk-button.uk-button-danger');
+    expect(button).toBeTruthy();
+    done();
+});
+
+test("a button must take a text argument", async(done) => {
+    initialize({textLabel: "Henlo"}, `
+aire-button(label.bind="textLabel", icon.bind="icon" text)
+  `);
+    await component.create(bootstrap);
+    let button = document.querySelector('.uk-button.uk-button-text');
+    expect(button).toBeTruthy();
+    done();
+});
+
+test("a button must take a link argument", async(done) => {
+    initialize({textLabel: "Henlo"}, `
+aire-button(label.bind="textLabel", icon.bind="icon" link)
+  `);
+    await component.create(bootstrap);
+    let button = document.querySelector('.uk-button.uk-button-link');
+    expect(button).toBeTruthy();
+    done();
+});
+
+test("a button must take a full argument", async(done) => {
+    initialize({textLabel: "Henlo"}, `
+aire-button(label.bind="textLabel", icon.bind="icon" full)
+  `);
+    await component.create(bootstrap);
+    let button = document.querySelector('.uk-button.uk-width-1-1');
+    expect(button).toBeTruthy();
+    done();
+});
+
+test("a button must take a small argument", async(done) => {
+    initialize({textLabel: "Henlo"}, `
+aire-button(label.bind="textLabel", icon.bind="icon" small)
+  `);
+    await component.create(bootstrap);
+    let button = document.querySelector('.uk-button.uk-button-small');
+    expect(button).toBeTruthy();
+    done();
+});
+
+test("a button must take a large argument", async(done) => {
+    initialize({textLabel: "Henlo"}, `
+aire-button(label.bind="textLabel", icon.bind="icon" large)
+  `);
+    await component.create(bootstrap);
+    let button = document.querySelector('.uk-button.uk-button-large');
+    expect(button).toBeTruthy();
+    done();
+});
+
+test("a button must take a square argument", async(done) => {
+    initialize({textLabel: "Henlo"}, `
+aire-button(label.bind="textLabel", icon.bind="icon" square)
+  `);
+    await component.create(bootstrap);
+    let button = document.querySelector('.uk-button.aire-button-square');
+    expect(button).toBeTruthy();
+    done();
 });
