51f8335cfa240ce27e7ac595f95e082e76ea9157
lisagumerman <lisajgumerman@gmail.com>
2018-12-22 02:43:36 -0700

    input tests. unable to get classes around icon to pass

--- aire/src/main/form/input.ts
+++ aire/src/main/form/input.ts
@@ -1,12 +1,16 @@
 import {bindable, customElement, inject} from "aurelia-framework";
 import {AireFormElement} from "./element";
+import {DOM} from "aurelia-pal";
 
-@inject(Element)
+@inject(DOM.Element)
 @customElement('aire-input')
 export class AireInput extends AireFormElement {
 
     @bindable
-    type        : string;
+    type        : string = "text";
+
+    @bindable
+    flip        : boolean;
 
     constructor(element: Element) {
         super(element);
