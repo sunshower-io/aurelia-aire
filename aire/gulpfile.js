bbbc4215474e3f59e5344ff9d74fddea62de0e37
Josiah Haswell <josiah.haswell@gmail.com>
2018-12-26 16:37:00 -0700

    fixing build by moving to aire-build

--- aire/gulpfile.js
+++ aire/gulpfile.js
@@ -1,9 +1,12 @@
 /**'
  * Register aliases (configured in package.json/_moduleAliases
  */
-// require('module-alias/register');
+// // require('module-alias/register');
+//
+// const gulp = require('gulp'),
+//     requireDirectory = require('require-dir'),
+//     tasks = requireDirectory('../build/tasks');
 
-const gulp = require('gulp'),
-    requireDirectory = require('require-dir'),
-    tasks = requireDirectory('../build/tasks');
+
+const aireBuild = require('@sunshower/aire-build');
 
