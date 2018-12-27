62bc1d8c14858d3b0c2336cfc87ad529d1d026a8
Josiah Haswell <josiah.haswell@gmail.com>
2018-12-27 15:18:52 -0700

    Updating

--- build/tasks/clean.js
+++
@@ -1,9 +0,0 @@
-module.paths.push(`${process.cwd()}/node_modules`);
-const gulp = require('gulp'),
-    paths = require('../paths'),
-    del = require('del'),
-    vinylPaths = require('vinyl-paths');
-
-gulp.task('clean', () =>  {
-    return gulp.src(paths.output).pipe(vinylPaths(del));
-});
