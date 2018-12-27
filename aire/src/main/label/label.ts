d33424aad0e7e53d0ed3babc6670edf23e01fe37
bodhibrdr <julie.gumerman@gmail.com>
2018-12-27 14:23:22 -0700

    Updated tests

--- aire/src/main/label/label.ts
+++ aire/src/main/label/label.ts
@@ -4,20 +4,20 @@ import {dom} from "aire/core";
 @inject(Element)
 @customElement('aire-label')
 export class AireLabel {
-    label: HTMLElement;
+    el: HTMLElement;
 
     @bindable
-    label: //add text here. not done yet!
+    label: string;
 
     constructor(private element: Element) {
 
 
     }
     attached() {
-        dom.decorateTo(this.element, this.label, "success", "uk-label-success");
-        dom.decorateTo(this.element, this.label, "default", "uk-label-default");
-        dom.decorateTo(this.element, this.label, "warning", "uk-label-warning");
-        dom.decorateTo(this.element, this.label, "danger", "uk.label.warning");
+        dom.decorateTo(this.element, this.el, "success", "uk-label-success");
+        dom.decorateTo(this.element, this.el, "default", "uk-label-default");
+        dom.decorateTo(this.element, this.el, "warning", "uk-label-warning");
+        dom.decorateTo(this.element, this.el, "danger", "uk-label-danger");
     }
 
 
--- aire/src/test/label/label.spec.ts
+++ aire/src/test/label/label.spec.ts
@@ -9,6 +9,59 @@ let component : any;
 
 
 function initialize(bindingContext : any, template : string) {
-    component = newComponent(template, bindingContext, 'label/label';
-    )
+    component = newComponent(template, bindingContext, 'label/label');
 }
+
+afterEach ( () => {
+    component.dispose();
+});
+
+test( "a label can be generated", async(done) => {
+    initialize( {}, `aire-label`);
+    await component.create(bootstrap);
+    let label = document.querySelector( '.uk-label');
+    expect(label).toBeTruthy();
+    done();
+});
+
+
+test( "a default label can appear", async(done) => {
+    initialize( {}, `aire-label(default)`);
+    await component.create(bootstrap);
+    let label = document.querySelector( '.uk-label-default');
+    expect(label).toBeTruthy();
+    done();
+});
+
+test( "a warning label can appear", async(done) => {
+    initialize( {}, `aire-label(warning)`);
+    await component.create(bootstrap);
+    let label = document.querySelector( '.uk-label-warning');
+    expect(label).toBeTruthy();
+    done();
+});
+
+
+test( "a danger label can appear", async(done) => {
+    initialize({}, `aire-label(danger)`);
+    await component.create(bootstrap);
+    let label = document.querySelector('.uk-label-danger');
+    expect(label).toBeTruthy();
+    done();
+});
+
+test( "a success label can appear", async(done) => {
+    initialize( {}, `aire-label(success)`);
+    await component.create(bootstrap);
+    let label = document.querySelector('.uk-label-success');
+    expect(label).toBeTruthy();
+    done();
+});
+
+test( "the desired string appears in the label", async(done) => {
+    initialize( {textLabel : "I am a label."}, `aire-label(label.bind="textLabel")`);
+    await component.create(bootstrap);
+    let label = document.querySelector( '.uk-label');
+    expect(label.textContent).toBe("I am a label.");
+    done();
+});
