e6dc1444196efff1e6ca44affe8d34ef9cf534f0
lisagumerman <lisajgumerman@gmail.com>
2018-12-28 14:12:07 -0700

    navbar-section docs

--- aire/src/main/navbar/section.ts
+++ aire/src/main/navbar/section.ts
@@ -2,6 +2,47 @@ import {inject, customElement} from "aurelia-framework";
 import {dom}                                  from "aire/core";
 import {DOM} from "aurelia-pal";
 
+/**
+ * The navbar-section aligns and organizes navbar items
+ *
+ * @component navbar-section
+ *
+ * @param {pseudo} left
+ * @param {pseudo} right
+ * @param {pseudo} center
+ *
+ * @section.usage
+ * @usage.title
+ * Usage
+ *
+ * @usage.description
+ * A navbar-section is a slot that can be left, right or center.
+ *
+ * @usage.examples.Left
+ * aire-navbar
+ *  navbar-section(left)
+ *    navbar-item Left
+ *
+ * @usage.examples.Right
+ * aire-navbar
+ *  navbar-section(right)
+ *    navbar-item Center
+ *
+ * @usage.examples.Center
+ * aire-navbar
+ *  navbar-section(center)
+ *    navbar-item Center
+ *
+ * @usage.examples.Multiple
+ * aire-navbar
+ *  navbar-section(left)
+ *    navbar-item Left
+ *  navbar-section(center)
+ *    navbar-item Center
+ *  navbar-section(right)
+ *    navbar-item right
+ *
+ */
 @inject(DOM.Element)
 @customElement('navbar-section')
 export class AireNavbarSection {
