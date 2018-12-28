2d976e902cafc6d2c0871afb96c067c36c4afbaf
lisagumerman <lisajgumerman@gmail.com>
2018-12-27 23:37:29 -0700

    icon docs

--- aire/src/main/form/textarea.ts
+++ aire/src/main/form/textarea.ts
@@ -18,6 +18,39 @@ import {DOM} from 'aurelia-pal';
  * @param {pseudo} blank
  *
  *
+ * @section.usage
+ * @usage.title
+ * Usage
+ * @usage.description
+ * Just like any form element, a textarea can have a label and a value. It can also be disabled.
+ *
+ * @usage.examples.Default
+ * aire-textarea
+ *
+ * @usage.examples.With_Label_And_Value
+ * aire-textarea(label="My Textarea" value="1234")
+ *
+ * @usage.examples.Disabled
+ * aire-textarea(disabled label="Disabled Textarea")
+ *
+ *
+ * @section.styles
+ * @styles.title
+ * Styles
+ * @styles.description
+ * An textarea's label can be moved horizontal with `horizontal`. The textarea can also have an `danger` or `success` state. For a more minimal look, make it `blank`.
+ *
+ * @styles.examples.Horizontal
+ * aire-textarea(horizontal label="Horizontal Textarea")
+ *
+ * @styles.examples.Danger
+ * aire-textarea(danger label="Dangerous Textarea")
+ *
+ * @styles.examples.Success
+ * aire-textarea(success label="Successful Textarea")
+ *
+ * @styles.examples.Blank
+ * aire-textarea(blank label="Minimal Textarea")
  */
 @inject(DOM.Element)
 @customElement('aire-textarea')
