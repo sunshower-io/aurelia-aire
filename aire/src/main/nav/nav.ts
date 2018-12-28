0ea862a0f21acf449ea4a514c1b38033fb5b5649
lisagumerman <lisajgumerman@gmail.com>
2018-12-28 13:34:20 -0700

    nav and navbar docs

--- aire/src/main/nav/nav.ts
+++ aire/src/main/nav/nav.ts
@@ -12,30 +12,42 @@ import {dom}              from "aire/core";
 
 /**
  *
- * this is a cool nav component
- *
- * what do you think?
+ * The aire-nav component is a styled list that takes a router to automatically lay out and link up navigation.
  *
  * @component nav
- *
- * @section.usage
- *
- * @usage.title Usage
- * @usage.description
- *  This is a cool bean
- *  #whatever _whatever_
- *
- * @usage.examples.Router
- *  aire-nav(router.bind="router")
- *
- * @usage.examples.OtherRouter
- *  aire-nav(router.bind="router")
- *
- *
+
  * @param {bindable} router (aurelia-router)
  * @param {bindable} normalizeTitle (f: NavModel => string)
  *
- * @param {pseudo} whatever
+ * @param {pseudo} default
+ * @param {pseudo} primary
+ * @param {pseudo} center
+ *
+ * @section.usage
+ * @usage.title
+ * Usage
+ * @usage.description
+ * Bind a router to have aire-nav iterate over NavModels and list out elements. To change how the text displays, pass in a normalizeTitle function.
+ *
+ * @usage.examples.Router
+ * aire-nav(router.bind="router")
+ *
+ * @section.style
+ * @style.title
+ * Style
+ * @style.description
+ * In addition to its relatively unstyled state, a nav can be made to be `default`, `primary` or `center`:
+ *
+ * @style.examples.Default
+ * aire-nav(router.bind="router" default)
+ *
+ * @style.examples.Primary
+ * aire-nav(router.bind="router" primary)
+ *
+ * @style.examples.Center
+ * aire-nav(router.bind="router" center)
+ *
+ *
  */
 @inject(EventAggregator, Element)
 @customElement('aire-nav')
