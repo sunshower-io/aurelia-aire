07daf6b24a81ada7449d0f88d75ba98bdf18eec1
bodhibrdr <julie.gumerman@gmail.com>
2018-12-27 19:45:15 -0700

    documentation

--- aire/src/main/label/label.ts
+++ aire/src/main/label/label.ts
@@ -1,6 +1,47 @@
 import {bindable, customElement, inject}    from "aurelia-framework";
 import {dom} from "aire/core";
 
+
+/**
+ *The aire-label is a span element with three optional messages and a fourth option to create your own.
+ *
+ *@component label
+ *
+ *
+ *@param {bindable} label (string)
+ * @param {pseudo} default
+ *@param {pseudo} success
+ * @param {pseudo} warning
+ * @param {pseudo} danger
+ *
+ *
+ *@section.usage
+ *@usage.title
+ * Usage
+ * @usage.description
+ *A label can contain words
+ *
+ * @usage.examples.label
+ *  aire-label(label="Edit")
+ *
+ * @section.style
+ * @style.title
+ * Style
+ * @style.description
+ * Labels can come up with warning, danger, success, or a user-chosen message.
+ *
+ * @usage.examples.Styles
+ * p
+ * aire-label(label="Default" default)
+ * p
+ * aire-label(label="Success" success)
+ * p
+ * aire-label(label="Danger" danger)
+ * p
+ * aire-label(label="Warning" warning)
+ *
+ */
+
 @inject(Element)
 @customElement('aire-label')
 export class AireLabel {
@@ -22,3 +63,5 @@ export class AireLabel {
 
 
 }
+
+
--- aire/src/main/tile/tile.ts
+++ aire/src/main/tile/tile.ts
@@ -2,6 +2,56 @@ import {bindable, customElement, inject} from "aurelia-framework";
 import {dom} from "aire/core/dom";
 
 
+/**
+ * The aire-tile is a freaking awesome little tile with four background options and three padding options.
+ *
+ * @component button
+ *
+ *
+ * @param {pseudo} default
+ * @param {pseudo} primary
+ * @param {pseudo} secondary
+ * @param {pseudo} muted
+ * @param {pseudo} no padding
+ * @param {pseudo} small padding
+ * @param {pseudo} large padding
+ *
+ * @asection.usage
+ * @usage.title
+ * Usage
+ * @usage.description
+ * A tile can have one of four backgrounds and three padding options.
+ *
+ * @usage.examples.Tile
+ * aire-tile(label="I am a tile.")
+ *
+ * @section.style
+ * @style.title
+ * Style
+ * @style.description
+ * Tiles have several styling options--default, primary, secondary, or muted--and three padding options: none, small, or large.
+ *
+ * @usage.examples.Styles
+ * p
+ * aire-tile(label="Default" default)
+ * p
+ * aire-tile(label="Muted" muted)
+ * p
+ * aire-tile(label="Primary" primary)
+ * p
+ * aire-tile(label="Secondary" secondary)
+ * p
+ * aire-tile(label="No Padding" padding-remove)
+ * p
+ * aire-tile(label="Small Padding" padding-small)
+ * p
+ * aire-tile(label="Large Padding" padding-large)
+ *
+ */
+
+
+
+
 @inject(Element)
 @customElement('aire-tile')
 export class AireTile {
@@ -22,3 +72,4 @@ export class AireTile {
         dom.decorateTo(this.element, this.div, "padding-large", "uk-padding-large");
     }
 }
+
