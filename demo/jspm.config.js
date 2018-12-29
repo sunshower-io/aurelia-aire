5acb399168bcd99c481c26be953e359f0244fd60
josiahhaswell <josiahhaswell@sunshower.io>
2018-12-29 13:56:55 +0000

    releasing [skip-build]

--- demo/jspm.config.js
+++ demo/jspm.config.js
@@ -40,7 +40,7 @@ SystemJS.config({
     "local:*.json"
   ],
   map: {
-    "aire": "npm:aire@0.0.52",
+    "aire": "npm:aire@0.0.53",
     "assert": "npm:jspm-nodelibs-assert@0.2.1",
     "aurelia-animator-velocity": "npm:aurelia-animator-velocity@1.2.0",
     "aurelia-binding": "npm:aurelia-binding@2.1.7",
@@ -431,11 +431,11 @@ SystemJS.config({
         "punycode": "npm:punycode@1.3.2"
       }
     },
-    "npm:aire@0.0.52": {
+    "npm:aire@0.0.53": {
       "map": {
         "aurelia-animator-velocity": "npm:aurelia-animator-velocity@1.2.0",
-        "aurelia-fetch-client": "npm:aurelia-fetch-client@1.7.0",
         "aurelia-bootstrapper": "npm:aurelia-bootstrapper@2.3.1",
+        "aurelia-fetch-client": "npm:aurelia-fetch-client@1.7.0",
         "uikit": "npm:uikit@3.0.0-rc.25"
       }
     }
