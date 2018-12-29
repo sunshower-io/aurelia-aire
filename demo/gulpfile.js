ea4a3de954f5fa103ce233a8efab3c00f0adc2f3
Josiah Haswell <josiah.haswell@gmail.com>
2018-12-29 03:56:10 -0700

    updating to released aire build

--- demo/gulpfile.js
+++ demo/gulpfile.js
@@ -1,7 +1,7 @@
-// const
-//     gulp = require('gulp');
-// require('@sunshower/aire-build')(gulp);
 const
-    gulp = require('gulp'),
-    requireDirectory = require('require-dir'),
-    aireBuild = require('../../aire-build/src/main/tasks/build.js')(gulp);
+    gulp = require('gulp');
+require('@sunshower/aire-build')(gulp);
+// const
+//     gulp = require('gulp'),
+//     requireDirectory = require('require-dir'),
+//     aireBuild = require('../../aire-build/src/main/tasks/build.js')(gulp);
